a30505da688b8557608b22d8f622f09bd162b432
yorickhardy <45015436+yorickhardy@users.noreply.github.com>
2022-09-02 06:53:50 +0200

    Fix path for xypic

--- xypic.js
+++ xypic.js
@@ -16769,4 +16769,4 @@ MathJax.Hub.Register.StartupHook("SVG Xy-pic Require",function () {
   MathJax.Hub.Startup.signal.Post("SVG Xy-pic Ready");
 });
 
-MathJax.Ajax.loadComplete("[MathJax]/extensions/TeX/xypic.js");
+MathJax.Ajax.loadComplete("https://github.com/yorickhardy/mdmathjaxtests/raw/main/xypic.js");
